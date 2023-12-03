f06f545962096e533642027b7ea0ef00ae39065d
Tino <Konstantin.v.milchev@gmail.com>
2023-12-03 15:25:09 +0000

    Added assertions, working on interfaces

---
+++ fundamentals/interfaces.go
@@ -0,0 +1,42 @@
+package fundamentals
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+type printer interface {
+	Print() string
+}
+
+type user struct {
+	username string
+	id       int
+}
+
+func (u user) Print() string {
+	return fmt.Sprintf("%v [%v]\n", u.username, u.id)
+}
+
+type menuItem struct {
+	name   string
+	prices map[string]float64
+}
+
+func (mi menuItem) Print() string {
+	var buffer bytes.Buffer
+	buffer.WriteString(mi.name)
+	buffer.WriteString(strings.Repeat("-", 10) + "\n")
+	for size, price := range mi.prices {
+		fmt.Fprintf(&buffer, "\t%10s%10.2f\n", size, price)
+	}
+
+	return buffer.String()
+}
+
+func main() {
+	var p printer
+	p = user{username: "adent", id: 14}
+	fmt.Println(p.Print())
+}
