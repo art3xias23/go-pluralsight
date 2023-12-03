919293ab6080eabc0771267aa4908b2b15adedf2
Tino <Konstantin.v.milchev@gmail.com>
2023-12-03 22:53:24 +0000

    added custom generic type

--- generics/main.go
+++ generics/main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	// maps()
+	genConcat()
+}
+
+func slices() {
+
 	initSlice := []float32{
 		12.3,
 		40.1,
@@ -13,11 +19,11 @@ func main() {
 	fmt.Println(&initSlice[0])
 	fmt.Println(&referenceSlice[0])
 
-	valueSlice := copy(initSlice)
+	valueSlice := copySlice(initSlice)
 	fmt.Println(&valueSlice[0])
 }
 
-func copy[V any](s []V) []V {
+func copySlice[V any](s []V) []V {
 	copy := make([]V, len(s))
 
 	for index, value := range s {
@@ -25,4 +31,50 @@ func copy[V any](s []V) []V {
 	}
 
 	return copy
+}
+
+func maps() {
+	myMap := map[float32]string{1: "Ben",
+		2: "Michael",
+		3: "Brian"}
+	copy := copyMaps(myMap)
+
+	fmt.Print(copy)
+
+}
+
+func copyMaps[K comparable, V any](m map[K]V) map[K]V {
+	copy := make(map[K]V, len(m))
+
+	for key, value := range m {
+		copy[key] = value
+	}
+
+	return copy
+}
+
+type addable interface {
+	int | float32 | string
+}
+
+func genConcat() {
+	integers := []int{1, 2, 3}
+	floats := []float32{1.3, 2.3, 3.4}
+	strs := []string{"I", "want", "ice-cream"}
+	result := concat(integers)
+	fmt.Println(result)
+
+	result2 := concat(floats)
+	fmt.Println(result2)
+
+	result3 := concat(strs)
+	fmt.Println(result3)
+}
+
+func concat[V addable](s []V) V {
+	var result V
+	for _, val := range s {
+		result += val
+	}
+	return result
 }
